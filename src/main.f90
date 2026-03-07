14c5d9fab44432a73f775f845bc919713fda3562
Valerio <valerio.luzzi@gecosistema.com>
2026-03-07 08:32:08 +0100

    Aggiungi gestione della versione e informazioni di compilazione

--- src/main.f90
+++ src/main.f90
@@ -1,4 +1,10 @@
 PROGRAM HelloWorld
-    ! This is a comment line
+    use module_version
+    implicit none
+    character(len=:), allocatable :: vstr
+
     PRINT *, "Hello World!"
+    vstr = get_version_string()
+    PRINT *, 'Version:', trim(vstr)
+
 END PROGRAM HelloWorld
